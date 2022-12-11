d25d5072b065828cc2013be5b2fdad8e0ce82904
nagarjunadu <dnag198@gmail.com>
2022-12-11 14:53:00 -0600

    Added StudentDB and navigationservices

--- StudentsRecords/Services/StudentDB.cs
+++ StudentsRecords/Services/StudentDB.cs
@@ -1,1 +1,50 @@
-﻿
+﻿using System;
+using SQLite;
+
+namespace StudentsRecords
+{
+    public class StudentDB
+    {
+        readonly SQLiteAsyncConnection database;
+        public StudentDB(string dbpath)
+        {
+            database = new SQLiteAsyncConnection(dbpath);
+            database.CreateTableAsync<Student>().Wait();
+        }
+        public Task<List<Student>> GetStudentsAsync()
+        {
+            return database.Table<Student>().ToListAsync();
+        }
+        public Task<Student> GetStudentAsync(int id)
+        {
+            return database.Table<Student>().Where(i => i.studentid == id).FirstOrDefaultAsync();
+        }
+
+        public Task<int> SaveStudentAsync(Student student)
+        {
+            if (student.studentid != 0)
+            {
+                return database.UpdateAsync(student);
+            }
+            else
+            {
+                return database.InsertAsync(student);
+            }
+        }
+
+        public Task<int> SaveStudentsAsync(List<Student> students)
+        {
+            database.Table<Student>().DeleteAsync();
+            return database.InsertAllAsync(students);
+        }
+        public Task<int> DeleteStudentAsync(Student student)
+        {
+            return database.DeleteAsync(student);
+        }
+
+        public Task<int> EditStudent(Student student)
+        {
+            return database.UpdateAsync(student);
+        }
+    }
+}
