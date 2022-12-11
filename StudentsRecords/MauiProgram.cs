2cd5ce88544008a16f8ffdc46b51c36cc74b569b
K683G868 <Kollisriharitha@gmail.com>
2022-12-11 15:20:46 -0600

    added mauics

--- StudentsRecords/MauiProgram.cs
+++ StudentsRecords/MauiProgram.cs
@@ -1,1 +1,41 @@
-﻿
+﻿using Microsoft.Extensions.Logging;
+using StudentsRecords.Contracts.Services;
+using StudentsRecords.Platforms.Android;
+using StudentsRecords.Services;
+using StudentsRecords.ViewModels;
+
+namespace StudentsRecords;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        builder.Services.AddTransient<MainPage>();
+        builder.Services.AddTransient<MainPageViewModel>();
+        builder.Services.AddTransient<AddStudent>();
+        builder.Services.AddTransient<AddStudentViewModel>();
+        builder.Services.AddTransient<EditStudent>();
+        builder.Services.AddTransient<EditStudentViewModel>();
+        builder.Services.AddTransient<Views.AboutPage>();
+        builder.Services.AddTransient<AboutPageViewModel>();
+        builder.Services.AddTransient<Views.SupportPage>();
+        builder.Services.AddTransient<SupportPageViewModel>();
+
+        builder.Services.AddSingleton<INavigationService, NavigationService>();
+        return builder.Build();
+    }
+}
+
