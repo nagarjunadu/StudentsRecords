77ecf36b8c17d3b250626189e3e1ffeee42077a8
K683G868 <Kollisriharitha@gmail.com>
2022-12-11 15:22:17 -0600

    addded student model

--- StudentsRecords/ViewModels/AddStudentViewModel.cs
+++ StudentsRecords/ViewModels/AddStudentViewModel.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+using SQLite;
+
+namespace StudentsRecords
+{
+    public class Student
+    {
+        [PrimaryKey, AutoIncrement]
+        public int studentid { get; set; }
+        [NotNull]
+        public string studentname { get; set; }
+        public string studentcourse { get; set; }
+        public int studentage { get; set; }
+    }
+}
