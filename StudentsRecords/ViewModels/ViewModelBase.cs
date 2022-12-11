34d6a9f2e3c0b8404a6a89d0d7fa1bc92ed70b59
K683G868 <Kollisriharitha@gmail.com>
2022-12-11 15:15:38 -0600

    added view model

--- StudentsRecords/ViewModels/ViewModelBase.cs
+++ StudentsRecords/ViewModels/ViewModelBase.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace StudentsRecords.ViewModels
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public virtual Task OnNavigatingTo(object? parameter)
+            => Task.CompletedTask;
+
+        public virtual Task OnNavigatedFrom(bool isForwardNavigation)
+            => Task.CompletedTask;
+
+        public virtual Task OnNavigatedTo()
+            => Task.CompletedTask;
+
+        public void OnPropertyChanged([CallerMemberName] string name = "") =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
