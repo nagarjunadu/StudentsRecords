799613bf50ee151e63e866b9502bc0d3b0fabd02
nagarjunadu <dnag198@gmail.com>
2022-12-11 14:55:26 -0600

    added AboutPagecs

--- StudentsRecords/ViewModels/AboutPageViewModel.cs
+++ StudentsRecords/ViewModels/AboutPageViewModel.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+namespace StudentsRecords.ViewModels
+{
+    public class AboutPageViewModel : ViewModelBase
+    {
+        readonly Contracts.Services.INavigationService _navigationService;
+        public AboutPageViewModel(Contracts.Services.INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+    }
+}
